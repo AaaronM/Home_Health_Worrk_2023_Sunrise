1ade1a283faa65e325831cc8481f0612b7523489
Aaron <aaron@Aarons-iMac.local>
2023-04-07 13:16:56 -0700

    some_design elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,18 @@ import React from "react";
 import "../CSS/Home.css";
 // ___________CSS_____________
 import mainpic from "../IMG/mainpic.png";
+import Thermometer from "../IMG/Thermometer.png";
+import Stetoscope from "../IMG/Stetoscope.png";
+import microskop from "../IMG/microskop.png";
+import MedicalRecord from "../IMG/MedicalRecord.png";
+
+import IllustrationTitle from "../IMG/IllustrationTitle.png";
+import FirstAid from "../IMG/FirstAid.png";
+import firstidkit from "../IMG/firstidkit.png";
+import pharmacy_1 from "../IMG/pharmacy_1.png";
+import stethoscope_1 from "../IMG/stethoscope_1.png";
+import sc from "../IMG/sc.png";
+
 // ___________IMG________________
 
 import { BsFillHeartPulseFill, BsFileMedical } from "react-icons/bs";
@@ -39,14 +51,20 @@ const Home = () => {
           <img className="home__img" src={mainpic} alt="pic5" />
           <div className="home__img__bg"></div>
           <div className="home__img__bg__2"></div>
+          {/* <img className="" src={Stetoscope} alt="Stetoscope" /> */}
         </div>
       </div>
+      <img className="firstidkit" src={firstidkit} alt="firstidkit" />
       <ServicesProvided />
       {/* // ______________SERVICES ________________ */}
       <DocList />
+      <img className="pharmacy_1" src={pharmacy_1} alt="pharmacy_1" />
       {/* // ______________DOCLIST ________________ */}
 
       <Consultation />
+      <img className="stethoscope_1" src={stethoscope_1} alt="stethoscope_1" />
+      <img className="sc" src={sc} alt="sc" />
+
       {/* // ______________ Consultation________________ */}
     </div>
   );
